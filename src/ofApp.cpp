f7d4a237ddb40a3f44873c88e3a2a959b31f8ec4
Robotechnic <robotechnic.developeur@gmail.com>
2020-09-13 12:23:14 +0200

    the camera follow the player

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,7 +5,7 @@ void ofApp::setup(){
     playerNameFont.load("00 Starmap Truetype.ttf",9);
     test = new Player(50,50,"sprites/Male/Male 01-1.png","Robotechnic");
 
-    cout<<"Load map: "<<m.loadMap("maps/startMap.tmx")<<endl;
+    cout<<"Load map: "<<m.loadMap("maps/test.tmx")<<endl;
 }
 
 //--------------------------------------------------------------
@@ -26,8 +26,17 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    //draw map and mobs
+    ofTranslate(-test->getX(),-test->getY());
     m.draw(0,0,ofGetWidth(),ofGetHeight());
+
+    //draw player
+    ofTranslate(ofGetWidth()/2-SPRITE_SIZE/2,ofGetHeight()/2-SPRITE_SIZE/2);
     test->draw(playerNameFont);
+
+    //draw fixed things (fps, ui...)
+    ofTranslate(test->getX()-ofGetWidth()/2+SPRITE_SIZE/2,test->getY()-ofGetHeight()/2+SPRITE_SIZE/2);
+    playerNameFont.drawString(ofToString(ofGetFrameRate()),10,10);
 }
 
 //--------------------------------------------------------------
